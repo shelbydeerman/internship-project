6a84ec504f019c19f251e7b6923fa4a7cc3fcab8
shelbydeerman <shelby.r.deerman@saic.com>
2021-06-09 09:01:55 -0500

    Add exact array size

--- src/project/Project.java
+++ src/project/Project.java
@@ -6,28 +6,44 @@ import java.util.*;
 
 public class Project {
     public static void main(String[] args) {
+
+        File fs = new File(args[0]);
+        int rowSize = 0;
+
+        // find the number of rows in the file by scanning each line
+        // and incrementing 'rowSize' each time
+        try (Scanner sc = new Scanner(fs)) {
+            while (sc.hasNext()) {
+                String temp = sc.nextLine();
+                rowSize++;
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found.");
+        }
+
+        // the number of meteors will be 1 less than the number of rows
+        int meteorNum = rowSize - 1;
+
+        // reopen the file and create an array big enough for the data
         File file = new File(args[0]);
-        String[][] meteorArray = new String[2000][11]; // fix to include bigger files
-        int meteorNum = 0;
+        String[][] meteorArray = new String[rowSize][10];
 
         try (Scanner scanner = new Scanner(file)) {
             for (int i = 0; scanner.hasNext(); i++) {
 
-                // scan the data into a 2d array and count the number of meteorites
+                // scan the data into the 2d array 'meteorArray'
                 String line = scanner.nextLine();
                 String[] array = line.split(",(?=([^\"]*\"[^\"]*\")*[^\"]*$)");
                 for (int j = 0; j < array.length; j++) {
                     meteorArray[i][j] = array[j];
                 }
-                meteorNum++;
             }
-            meteorNum = meteorNum - 1;
 
-            // iterate through the array to get the sum of the masses
+            // iterate through the array to get the sum of the masses of all the meteors
             double massSum = 0;
             int meteorWithMassNum = 0;
-            for (int i = 1; i < meteorNum + 1; i++) {
-                if (!meteorArray[i][4].equals("")) {
+            for (int i = 1; i < rowSize; i++) {
+                if (!meteorArray[i][4].equals("")) { //
                     double num = Double.parseDouble(meteorArray[i][4]);
                     massSum += num;
                     meteorWithMassNum++;
@@ -39,10 +55,10 @@ public class Project {
 
             // count the dates that repeat and save the number of repeats in "repeatDates"
             int[] repeatDates = new int[meteorArray.length];
-            for (int i = 1; i < meteorNum + 1; i++) {
-                for (int j = 0; j < meteorNum + 1; j++) {
-                    if (meteorArray[i][6].equals(meteorArray[j][6]) &&
-                            !meteorArray[j][6].equals("") && i != j) {
+            for (int i = 1; i < rowSize; i++) {
+                for (int j = 0; j < rowSize; j++) {
+                    if (!meteorArray[j][6].equals(" ") && i != j &&
+                            meteorArray[i][6].equals(meteorArray[j][6])) {
                         repeatDates[i]++;
                     }
                 }
@@ -58,6 +74,8 @@ public class Project {
                 }
             }
 
+            // print out all the data that was found ('meteorNum',
+            // 'massAveragePounds', 'year')
             System.out.println("Total number of meteorites: " + meteorNum);
             System.out.println("Average weight (mass) of meteorites: " + massAveragePounds + " lbs");
             System.out.println("Year with the most meteorites: " + year);
@@ -65,6 +83,5 @@ public class Project {
         } catch (FileNotFoundException e) {
             System.out.println("No file found.");
         }
-
     }
 }
